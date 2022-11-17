73b0538f18a088ec223c2ffcc6daca73b6f48f6f
ptr-ito <ptr2839@gmail.com>
2022-11-17 19:01:54 +0900

    Add: ログイン日時などの情報をトラッキングする設定を追加 #13

--- api/db/migrate/20221117095432_devise_token_auth_create_users.rb
+++ api/db/migrate/20221117095432_devise_token_auth_create_users.rb
@@ -37,6 +37,13 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       ## Tokens
       t.text :tokens
 
+      ## Trackable Info
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string  :current_sign_in_ip
+      t.string  :last_sign_in_ip
+
       t.timestamps
     end
 
