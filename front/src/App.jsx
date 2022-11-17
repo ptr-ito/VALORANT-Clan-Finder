c332d26570a67062d6205ddc88dd70c341d568a4
ptr-ito <ptr2839@gmail.com>
2022-11-17 15:47:28 +0900

    Add: ログインボタンと新規登録ボタンにLinkを追加

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/layouts/Header";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </>
   );
 };
