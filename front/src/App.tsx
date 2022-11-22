cf8d9c144d3c2243c0fe999f1eb990313cf650b6
ptr-ito <ptr2839@gmail.com>
2022-11-23 00:00:29 +0900

    Fix: 修正

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Header from "components/layouts/Header";
-import Post from "components/pages/post/Post";
+import Post from "components/pages/Post";
 import { Router } from "router/Router";
 
 const App = () => {
   return (
     <>
-      <Header>
-        <Router />
-      </Header>
+      <Router />
     </>
   );
 };
