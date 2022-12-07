d30fdd860897f2d38e89f7844c51403a66ec5883
ptr-ito <ptr2839@gmail.com>
2022-12-07 09:53:51 +0900

    Add: uuid動作確認

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
+  include CreateUuid
   primary_abstract_class
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_095322) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_004651) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "uuid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uuid"], name: "index_posts_on_uuid"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "sub"
     t.datetime "created_at", null: false
