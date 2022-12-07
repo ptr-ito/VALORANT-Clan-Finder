951bb74bd2b7cbee2a4750c64615783e4e535060
ptr-ito <ptr2839@gmail.com>
2022-12-07 09:21:11 +0900

    Remove: auth0動作検証用に作成したモデルとコントローラーを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_113929) do
-  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "title"
-    t.text "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_095322) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "sub"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "posts", "users"
 end
