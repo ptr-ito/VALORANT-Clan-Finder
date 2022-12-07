09dbbfa5de0953490bbbdab3642181775af1a982
ptr-ito <ptr2839@gmail.com>
2022-12-07 10:44:00 +0900

    Remove: uuid動作確認用モデル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_011443) do
-  create_table "articles", id: { type: :string, limit: 36 }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["id"], name: "index_articles_on_id"
-  end
-
-  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "uuid", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["uuid"], name: "index_posts_on_uuid"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_095322) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "sub"
     t.datetime "created_at", null: false
