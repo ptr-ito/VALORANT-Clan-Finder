1c4cfa7415411d0e5350fec0dab33861ec30a87e
ptr-ito <ptr2839@gmail.com>
2022-12-08 09:55:43 +0900

    Add: FactoryBotにテストデータを追加

--- app/models/mode.rb
+++ app/models/mode.rb
@@ -3,9 +3,8 @@ class Mode < ActiveHash::Base
   has_many :matches
 
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: 'コンペティティブ' },
-    { id: 3, name: 'アンレート' },
-    { id: 4, name: 'その他' }
+    { id: 1, name: 'コンペティティブ' },
+    { id: 2, name: 'アンレート' },
+    { id: 3, name: 'その他' }
   ]
 end
