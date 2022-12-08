0e09d6ddf243aa2d898671950532a4e994e6a9a2
ptr-ito <ptr2839@gmail.com>
2022-12-08 21:14:20 +0900

    Add: Matchesコントローラーにアクションを定義 #23

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::MatchesController < ApplicationController
+  def indx
+    matches = Match.all
+    render_json = MatchListSerializer.new(matches).serializable_hash.to_json
+    render json: render_json, status: :ok
+  end
+
+  def show
+    matche = Match.find_by!(id: params[:id])
+    render_json = MatchListSerializer.new(match).serializable_hash.to_json
+    render json: render_json, status: :ok
+  end
 end
