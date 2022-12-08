4f35aa2c055f3174436bef8be435b59953f2a008
ptr-ito <ptr2839@gmail.com>
2022-12-08 14:24:26 +0900

    Add: skip_routes: true, helper: false #23

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,10 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+    end
   end
 end
