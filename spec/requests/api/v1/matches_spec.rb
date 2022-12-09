8b8ead02296d2a4e7510c4d657f7474ea57ce907
ptr-ito <ptr2839@gmail.com>
2022-12-09 18:05:20 +0900

    Add: システムスペックを追加 #23

--- spec/requests/api/v1/matches_spec.rb
+++ spec/requests/api/v1/matches_spec.rb
@@ -1,23 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "非ログインユーザーのマッチ募集閲覧機能", type: :request do
+RSpec.describe 'Api::V1::Matches', type: :request do
   let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' } }
 
-  describe "非ログインユーザーが募集の投稿を閲覧できること" do
+  describe "マッチ募集投稿一覧 GET /api/v1/matches" do
     let(:published_match_num) { 5 }
     let(:closed_match_num) { 5 }
     let(:http_request) { get api_v1_matches_path, headers: headers }
     before do
-      binding.pry
       create_list(:match, published_match_num, :published)
       create_list(:match, closed_match_num, :closed)
     end
+    context '非ログインユーザー' do
+      it '投稿一覧を閲覧できる' do
+        http_request
+        expect(body['data'].count).to eq(published_match_num + closed_match_num)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 
-    it '非ログインユーザーが公開された募集投稿のみを閲覧できること' do
-      http_request
-      expect(body['data'].count).not_to eq(published_match_num + closed_match_num)
-      expect(response).to be_successful
-      expect(response).to have_http_status(:ok)
+  describe 'マッチ募集投稿詳細 GET /api/v1/mathes/:id' do
+    let!(:match) { create(:match, :published) }
+    let(:http_request) { get api_v1_match_path(match.id), headers: headers }
+    context '非ログインユーザー' do
+      it '投稿詳細を閲覧できる' do
+        http_request
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
