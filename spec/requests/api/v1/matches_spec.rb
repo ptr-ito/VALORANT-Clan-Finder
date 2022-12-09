b710059b29a75f3566c3b73de511e823d35695a4
ptr-ito <ptr2839@gmail.com>
2022-12-09 09:40:34 +0900

    Add: jsonヘルパーを作成 #23

--- spec/requests/api/v1/matches_spec.rb
+++ spec/requests/api/v1/matches_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Matches", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "非ログインユーザーのマッチ募集閲覧機能", type: :request do
+  let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json' } }
+
+  describe "非ログインユーザーが募集の投稿を閲覧できること" do
+    let(:published_match_num) { 5 }
+    let(:closed_match_num) { 5 }
+    let(:http_request) { get api_v1_matches_path, headers: headers }
+    before do
+      create_list(:match, published_match_num, :published)
+      create_list(:closed_match, closed_match_num, :closed)
+    end
+
+    it '非ログインユーザーが公開された募集投稿のみを閲覧できること' do
+      http_request
+      expect(body['data'].cont).not_to eq(published_match_num + closed_match_num)
+      expect(response).to be_successful
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
