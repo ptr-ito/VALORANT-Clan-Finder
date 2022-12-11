dfa47bb5160f17708036f59ff9ed4a35aef40a08
ptr-ito <ptr2839@gmail.com>
2022-12-11 21:07:07 +0900

    Remove: テーブルをリセット #25

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_015226) do
-  create_table "matches", id: { type: :string, limit: 36, comment: "プライマリキー" }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body"
-    t.integer "status", limit: 1, default: 0, null: false
-    t.string "user_id", null: false
-    t.integer "rank_id", null: false
-    t.integer "mode_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["id"], name: "index_matches_on_id"
-    t.index ["user_id"], name: "index_matches_on_user_id"
-  end
-
-  create_table "users", id: { type: :string, limit: 36, comment: "プライマリキー" }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "sub", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["id"], name: "index_users_on_id"
-    t.index ["sub"], name: "index_users_on_sub"
-  end
-
-  add_foreign_key "matches", "users"
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
