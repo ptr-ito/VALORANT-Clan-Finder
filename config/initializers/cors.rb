e230897c1c7def6dec3416040086938dbfc18ada
ptr-ito <ptr2839@gmail.com>
2022-12-11 21:41:59 +0900

    Add: corsに設定を追加 #25

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3001'
 
     resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+      headers: :any,
+      expose: %i[access-token expiry token-type uid client],
+      methods: %i[get post put patch delete options head]
   end
 end
