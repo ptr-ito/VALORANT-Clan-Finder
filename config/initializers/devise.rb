5991627fc32c605a76cff4620d9a377cacbb729f
ptr-ito <ptr2839@gmail.com>
2022-12-12 11:30:40 +0900

    Add: 新規登録とメール変更の際のメールフォームを作成 #26

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -28,7 +28,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-  config.mailer = 'Users::Mailer'
+  config.mailer = 'Users::Mailer' # Mailerをオーバーライド
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
@@ -152,7 +152,7 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.confirm_within = 3.days
+  config.confirm_within = 1.days # 認証メールの有効期限を24時間以内に設定
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
