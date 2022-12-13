4e823f4b8a721281c712ffcf0ab4ba75406baac3
ptr-ito <ptr2839@gmail.com>
2022-12-13 16:26:09 +0900

    Add: ログイン情報に name を追加 #26

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    sequence(:name, "username_1")
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
