b031ba98762e622f749389bc52c99f635c803ce1
ptr-ito <ptr2839@gmail.com>
2022-12-16 21:59:41 +0900

    Add: ルーティングにprofileを追加 #28

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resource :profile, only: %i[show edit update]
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
       }
