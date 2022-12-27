f748587e1ef91362ab2b1b25a6b378bc77ed6e43
ptr-ito <ptr2839@gmail.com>
2022-12-27 20:43:10 +0900

    Add: 各モデルにアソシエーションを定義 #29

--- app/models/mode.rb
+++ app/models/mode.rb
@@ -1,10 +1,11 @@
 class Mode < ActiveHash::Base
   include ActiveHash::Associations
-  has_many :matches, dependent: :destroy
+  has_many :match_posts, dependent: :destroy
 
   self.data = [
     { id: 1, name: 'コンペティティブ' },
     { id: 2, name: 'アンレート' },
+    { id: 2, name: 'カスタム' },
     { id: 3, name: 'その他' },
   ]
 end
