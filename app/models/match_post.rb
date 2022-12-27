b61e67dda4f7ce386375dfa697e62dc7f78397c4
ptr-ito <ptr2839@gmail.com>
2022-12-27 20:34:06 +0900

    Add: Match_postモデル追加 #29

---
+++ app/models/match_post.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: match_posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)      not null
+#  status     :integer          default(0), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  mode_id    :integer          default(1), not null
+#  mood_id    :integer          default(1), not null
+#  rank_id    :integer          default(1), not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_match_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class MatchPost < ApplicationRecord
+  belongs_to :user
+end
