5a24ba4cdb5aeeeecd2641b3a7f83d0887443b3e
ptr-ito <ptr2839@gmail.com>
2023-01-10 11:48:06 +0900

    Add: ランク情報を追加 #29

--- app/serializers/match_post_serializer.rb
+++ app/serializers/match_post_serializer.rb
@@ -1,9 +1,31 @@
+# == Schema Information
+#
+# Table name: match_posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)      not null
+#  status     :integer          default("published"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  mode_id    :integer          not null
+#  mood_id    :integer          not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_match_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class MatchPostSerializer
   include JSONAPI::Serializer
-  attributes :id, :content, :status
+  extend ActionView::Helpers::DateHelper
+  attributes :id, :content, :status, :created_at, :user_id
 
-attribute :rank do |object|
-    object.rank.name.to_s
+  attribute :rank do |object|
+    object.ranks.map{ |v| v.name}
   end
 
   attribute :mode do |object|
@@ -20,5 +42,13 @@ attribute :rank do |object|
 
   attribute :user_image do |object|
     object.user.image
+  end
+
+  attribute :created_at do |object|
+    if object.created_at > 1.day.ago
+      time_ago_in_words(object.created_at, include_seconds: true)
+    else
+      object.created_at.strftime("%F")
+    end
   end
 end
