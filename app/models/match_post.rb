2ba18e1b4aadb2f70c4a4c58b27a6265f96c05b6
ptr-ito <ptr2839@gmail.com>
2023-01-10 11:45:41 +0900

    Fix: アソシエーションを見直し #29

--- app/models/match_post.rb
+++ app/models/match_post.rb
@@ -4,12 +4,11 @@
 #
 #  id         :bigint           not null, primary key
 #  content    :text(65535)      not null
-#  status     :integer          default(0), not null
+#  status     :integer          default("published"), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  mode_id    :integer          default(1), not null
-#  mood_id    :integer          default(1), not null
-#  rank_id    :integer          default(1), not null
+#  mode_id    :integer          not null
+#  mood_id    :integer          not null
 #  user_id    :bigint           not null
 #
 # Indexes
@@ -22,16 +21,17 @@
 #
 class MatchPost < ApplicationRecord
   belongs_to :user
+  has_many :match_ranks, dependent: :destroy
+  has_many :ranks, through: :match_ranks
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :rank
+  # belongs_to_active_hash :rank
   belongs_to_active_hash :mood
   belongs_to_active_hash :mode
 
   validates :content, presence: true, length: { maximum: 140 }
-  validates :rank_id, presence: true
   validates :mode_id, presence: true
   validates :mood_id, presence: true
 
-  enum :status, { published: 0, closed: 1, trashed: 2,}
+  enum :status, { published: 0, closed: 1}
 end
