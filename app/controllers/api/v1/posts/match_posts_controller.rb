766053d981ebc4d88408d6d721ae3302c3c3ea95
ptr-ito <ptr2839@gmail.com>
2023-01-10 12:26:29 +0900

    Fix: コメントアウトを削除 #29

--- app/controllers/api/v1/posts/match_posts_controller.rb
+++ app/controllers/api/v1/posts/match_posts_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::Posts::MatchPostsController < ApplicationController
   before_action :authenticate_api_v1_user!, only: %i[create update destroy]
 
   def index
-    # render json: {match_posts: MatchPost.all.order("created_at DESC") }
     match_posts = MatchPost.all.published.includes(:user, :ranks).order(created_at: :desc)
     render_json = MatchPostSerializer.new(match_posts).serializable_hash.to_json
     render json: render_json, status: :ok
@@ -16,9 +15,7 @@ class Api::V1::Posts::MatchPostsController < ApplicationController
 
   def create
     match_post = current_api_v1_user.match_posts.build(match_post_params)
-    # ranks = Rank.where(id: params[:rank_id].split(','))
     if match_post.save
-      # match_post.ranks << ranks
       json_string = MatchPostSerializer.new(match_post).serializable_hash.to_json
       render json: json_string, status: :ok
     else
