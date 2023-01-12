fbc5f6ea7584fc6adcd865bb78505a8c7f0e4f13
ptr-ito <ptr2839@gmail.com>
2023-01-12 12:54:39 +0900

    Add: deleteアクション・updateアクションを追加 #30

--- app/controllers/api/v1/posts/match_post_comments_controller.rb
+++ app/controllers/api/v1/posts/match_post_comments_controller.rb
@@ -11,9 +11,27 @@ class Api::V1::Posts::MatchPostCommentsController < ApplicationController
     end
   end
 
+  def update
+    comment = MatchPostComment.find(params[:id])
+    if comment.update(comment_params)
+      json_string = MatchPostCommentSerializer.new(comment).serializable_hash.to_json
+      render json: json_string, status: :ok
+    else
+      render json: json_string.errors, status: :bad_request
+    end
+  end
+
+  def destroy
+    comment = current_api_v1_user.match_post_comments.find(params[:id])
+    comment.destroy!
+    comment = MatchPostComment.all
+    json_string = MatchPostCommentSerializer.new(comment).serializable_hash.to_json
+    render json: json_string, status: :ok
+  end
+
   private
 
   def comment_params
-    params.permit(:content).merge(match_post_id: params[:match_post_id])
+    params.permit(:id, :content, :parent_id).merge(match_post_id: params[:match_post_id])
   end
 end
