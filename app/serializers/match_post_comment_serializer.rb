b09c726d8723a1163769068881484ff243d5c323
ptr-ito <ptr2839@gmail.com>
2023-01-12 12:59:04 +0900

    Add: コメントの親子関係を追加 #30

--- app/serializers/match_post_comment_serializer.rb
+++ app/serializers/match_post_comment_serializer.rb
@@ -1,7 +1,31 @@
+# == Schema Information
+#
+# Table name: match_post_comments
+#
+#  id            :bigint           not null, primary key
+#  content       :string(255)      not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  match_post_id :bigint           not null
+#  parent_id     :bigint
+#  user_id       :bigint           not null
+#
+# Indexes
+#
+#  index_match_post_comments_on_match_post_id  (match_post_id)
+#  index_match_post_comments_on_parent_id      (parent_id)
+#  index_match_post_comments_on_user_id        (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (match_post_id => match_posts.id)
+#  fk_rails_...  (parent_id => match_post_comments.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class MatchPostCommentSerializer
   include JSONAPI::Serializer
   extend ActionView::Helpers::DateHelper
-  attributes :id, :content, :created_at, :user_id, :match_post_id
+  attributes :id, :content, :created_at, :user_id, :match_post_id, :parent_id
 
   attribute :created_at do |object|
     if object.created_at > 1.day.ago
