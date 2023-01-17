02a7373c6e1d1cbc344440b24ba9dfaf7f1bbaa6
ptr-ito <ptr2839@gmail.com>
2023-01-17 15:47:09 +0900

    Fix: テーブル周りを修正 #39

--- db/migrate/20230116002012_add_default_to_user_agent.rb
+++ db/migrate/20230116002012_add_default_to_user_agent.rb
@@ -1,6 +1,6 @@
 class AddDefaultToUserAgent < ActiveRecord::Migration[7.0]
   def change
-    change_column_default(:user_agents, :agent_id, from: nil, to: 0)
-    change_column_default(:user_agents, :user_id, from: nil, to: 0)
+    change_column_default(:user_agents, :agent_id, from: nil, to: 1)
+    change_column_default(:user_agents, :user_id, from: 0, to: nil)
   end
 end
