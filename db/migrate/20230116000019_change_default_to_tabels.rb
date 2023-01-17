c3b757e9fb4f42661708e7586598d19bd2479b10
ptr-ito <ptr2839@gmail.com>
2023-01-17 15:47:09 +0900

    Fix: テーブル周りを修正 #39

--- db/migrate/20230116000019_change_default_to_tabels.rb
+++ db/migrate/20230116000019_change_default_to_tabels.rb
@@ -1,6 +1,6 @@
 class ChangeDefaultToTabels < ActiveRecord::Migration[7.0]
   def change
-    change_column_default(:users, :rank_id, from: 1, to: 0)
-    change_column_default(:users, :highest_rank_id, from: 1, to: 0)
+    change_column_default(:users, :rank_id, from: 0, to: 1)
+    change_column_default(:users, :highest_rank_id, from: 0, to: 1)
   end
 end
