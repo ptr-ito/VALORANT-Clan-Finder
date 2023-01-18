539ec5c44b40f6c6edaedc0948f6fb0afcd348b8
ptr-ito <ptr2839@gmail.com>
2023-01-18 15:31:19 +0900

    Add: コメント機能をポリモーフィック関連に変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,33 +2,29 @@
 #
 # Table name: comments
 #
-#  id            :bigint           not null, primary key
-#  content       :string(255)      not null
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  match_post_id :bigint           not null
-#  parent_id     :bigint
-#  root_id       :bigint
-#  user_id       :bigint           not null
+#  id               :bigint           not null, primary key
+#  commentable_type :string(255)      not null
+#  content          :text(65535)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_id   :bigint           not null
+#  root_id          :bigint
+#  user_id          :bigint           not null
 #
 # Indexes
 #
-#  index_comments_on_match_post_id  (match_post_id)
-#  index_comments_on_parent_id      (parent_id)
-#  index_comments_on_root_id        (root_id)
-#  index_comments_on_user_id        (user_id)
+#  index_comments_on_commentable  (commentable_type,commentable_id)
+#  index_comments_on_root_id      (root_id)
+#  index_comments_on_user_id      (user_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (match_post_id => match_posts.id)
-#  fk_rails_...  (parent_id => comments.id)
 #  fk_rails_...  (root_id => comments.id)
-#  fk_rails_...  (user_id => users.id)
 #
 class Comment < ApplicationRecord
+  belongs_to :commentable, polymorphic: true
   belongs_to :user
-  belongs_to :match_post
   validates :content, presence: true, length: { maximum: 65_535 }
 
-  has_many :replies, class_name: 'Comment', foreign_key: :root_id, dependent: :destroy # rubocop:disable Rails/InverseOf
+  has_many :replies, class_name: 'Comment', foreign_key: :root_id, dependent: :destroy
 end
