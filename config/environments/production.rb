dcc06a7faf43a1b7624ca8d8ec05df0e90edec6d
ptr-ito <ptr2839@gmail.com>
2023-01-20 00:58:54 +0900

    Add: 本番環境用のメールアドレスの設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,21 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  host = ENV["DEPLOY_APP_NAME"]
+  config.action_mailer.default_url_options = { host: host, protocol: "https" }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: ENV["SMTP_ADDRESS"],
+  port: ENV['SMTP_PORT'],
+  domain: ENV['SMTP_DOMAIN'],
+  user_name: ENV['SMTP_USER_NAME'],
+  password: ENV['SMTP_PASSWD'],
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
