3babecf8611c42fc451c74f2cd6e08e8f2efff8e
ptr-ito <ptr2839@gmail.com>
2023-01-21 23:09:43 +0900

    Add: rescue_fromを記述してUnsafeRedirectErrorの様子を見る

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
   helper_method :current_user, :user_signed_in?
+
+  rescue_from ActionController::Redirecting::UnsafeRedirectError do
+    redirect_to 'https://valorant-finder.web.app'
+  end
 end
