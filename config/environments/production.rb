bd562fcc4eaad6dddb608efef86d789f3edab830
ptr-ito <ptr2839@gmail.com>
2023-01-21 17:22:39 +0900

    Fix: メールアドレスの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
-    port: 465,
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWD'],
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['EMAIL_ADDRESS'],
+    password: ENV['EMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true,
   }
