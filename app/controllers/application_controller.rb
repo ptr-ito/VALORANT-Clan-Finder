c2153545868520155de879debc75488ed45d3bfc
ptr-ito <ptr2839@gmail.com>
2023-01-21 21:53:50 +0900

    Add: フロントのURLを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
   helper_method :current_user, :user_signed_in?
-
-  rescue_from ActionController::Redirecting::UnsafeRedirectError do
-    redirect_to 'https://valofinder.com/mypage?account_confirmation_success=true'
-  end
-
 end
