82d9e4a1cfdd52561b1f2c47dd5b8e8b234a0538
ptr-ito <ptr2839@gmail.com>
2023-01-24 14:46:16 +0900

    Add: 画像の保存先をaws-s3に変更 #81

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,23 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  config.asset_host = 'http://localhost:3000'
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'rails-profile'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-profile'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+    # config.cache_storage = :fog
+  else
+    # 開発環境はlocalに保存
+    config.storage :file
+    config.enable_processing = false if Rails.env.test? 
+  end
 end
