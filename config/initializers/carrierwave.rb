4f9effd82e74f5bd9ebb04fcbab831375bb3fd62
ptr-ito <ptr2839@gmail.com>
2023-01-24 16:22:32 +0900

    rubocop -a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,18 +6,18 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'rails-profile'
+    config.fog_directory = 'rails-profile'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-profile'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
     }
     # config.cache_storage = :fog
   else
     # 開発環境はlocalに保存
     config.storage :file
-    config.enable_processing = false if Rails.env.test? 
+    config.enable_processing = false if Rails.env.test?
   end
 end
